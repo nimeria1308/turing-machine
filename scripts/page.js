a03f34b3632c1e27a939e851814ec166f09602ef
Simona Dimitrova <nimeria1308@gmail.com>
2022-01-08 22:03:13 +0200

    simplify animation code

--- scripts/page.js
+++ scripts/page.js
@@ -1,4 +1,12 @@
 var machine = null;
+var animation = null;
+
+function set_contents(element, contents) {
+    while (element.lastElementChild) {
+        element.removeChild(element.lastElementChild);
+    }
+    element.appendChild(contents);
+}
 
 function on_turing_json_selected(file_input) {
     const f = file_input.files[0];
@@ -13,43 +21,27 @@ function on_turing_json_selected(file_input) {
                 j.symbols, j.empty_symbol,
                 j.rules);
 
-            function set_contents(element, contents) {
-                while (element.lastElementChild) {
-                    element.removeChild(element.lastElementChild);
-                }
-                element.appendChild(contents);
-            }
-
             const machine_div = document.getElementById("machine");
             const tape_div = document.getElementById("tape");
 
-            function update_machine(graph, tape) {
-                set_contents(machine_div, graph);
-                set_contents(tape_div, tape);
-                setTimeout(render_machine, 100);
-            }
-
             function render_machine() {
-                if (machine.advance()) {
-                    machine.renderGraph()
-                        .then((graph) => {
-                            const tape = machine.renderTape();
-                            update_machine(graph, tape);
-                        })
-                        .catch(error => {
-                            console.error(error);
-                        });
+                machine.renderGraph()
+                    .then((graph) => {
+                        const tape = machine.renderTape();
+                        set_contents(machine_div, graph);
+                        set_contents(tape_div, tape);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+
+                if (!machine.advance()) {
+                    clearInterval(animation);
                 }
             }
 
-            machine.renderGraph()
-                .then((graph) => {
-                    const tape = machine.renderTape();
-                    update_machine(graph, tape);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
+            clearInterval(animation);
+            animation = setInterval(render_machine, 100);
         };
 
         reader.readAsText(f);
