fc742998777f3b619c701c006c90cf1c33459829
Simona Dimitrova <nimeria1308@gmail.com>
2022-01-11 22:51:25 +0200

    added bootstrap form

--- scripts/page.js
+++ scripts/page.js
@@ -8,6 +8,20 @@ function set_contents(element, contents) {
     element.appendChild(contents);
 }
 
+function file_changed(file) {
+    const f = file.files[0];
+    if (f) {
+        const source = document.getElementById("machine_source");
+        if (!source.value || confirm("Are you sure you want to replace the source?")) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                source.value = event.target.result;
+            };
+            reader.readAsText(f);
+        }
+    }
+}
+
 function run_turing_machine_file() {
     const file_input = document.getElementById("file");
 
